a624575f5222ef07a9979c6beb11f3ea5e66166d
KKOsses <kkos3dot@gmail.com>
2024-08-13 21:49:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,3 +21,12 @@
                 }
         }
 ?>
+<?php session_start(); /* Starts the session */
+
+if(!isset($_SESSION['UserData']['Username'])){
+        header("login.php");
+        exit;
+}
+?>
+
+Congratulation! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.
